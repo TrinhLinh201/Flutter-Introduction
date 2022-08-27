fa3f8a4da2a470c69e86385b87b3788650894e91
TrinhLinh201 <97020160+TrinhLinh201@users.noreply.github.com>
2022-08-27 19:45:44 +0700

    Update main.dart

--- calculator/lib/main.dart
+++ calculator/lib/main.dart
@@ -6,43 +6,17 @@ void main() {
   runApp(const MyApp());
 }
 
-bool check = true;
-
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MyHomePage();
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
     return MaterialApp(
+      darkTheme: ThemeData.dark(),
       title: "Máy tính",
-      theme: check ? ThemeData.light() : ThemeData.dark(),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("<== click để đổi theme"),
-          leading: IconButton(
-            icon: const Icon(Icons.menu),
-            onPressed: () {
-              setState(() {
-                check = !check;
-              });
-            },
-          ),
-        ),
-        body: const Home(),
+      theme: ThemeData.light(),
+      home: const Scaffold(
+        body: Home(),
       ),
     );
   }
